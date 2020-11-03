b87c829604af85b95ace8353654185e0ba302737
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-11-03 21:06:33 +0100

    Login form fixed.

--- src/components/home/LoginForm.js
+++ src/components/home/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Modal } from "rsuite";
 import "../../../node_modules/firebaseui/dist/firebaseui.css";
 
@@ -6,25 +6,26 @@ const firebase = require("firebase");
 const firebaseui = require("firebaseui");
 
 const LoginForm = ({ show, close }) => {
-  useEffect(() => {
-    const uiConfig = {
-      signInSuccessUrl: "/",
-      signInFlow: "popup",
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      ],
-    };
+  if (show) {
+    setTimeout(() => {
+      const uiConfig = {
+        signInSuccessUrl: "/",
+        signInFlow: "popup",
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        ],
+      };
 
-    if (firebaseui.auth.AuthUI.getInstance()) {
-      const ui = firebaseui.auth.AuthUI.getInstance();
-      ui.start("#firebaseui-auth-container", uiConfig);
-    } else {
-      const ui = new firebaseui.auth.AuthUI(firebase.auth());
-      ui.start("#firebaseui-auth-container", uiConfig);
-    }
-  }, []);
+      if (firebaseui.auth.AuthUI.getInstance()) {
+        const ui = firebaseui.auth.AuthUI.getInstance();
+        ui.start("#firebaseui-auth-container", uiConfig);
+      } else {
+        const ui = new firebaseui.auth.AuthUI(firebase.auth());
+        ui.start("#firebaseui-auth-container", uiConfig);
+      }
+    }, 1);
+  }
 
   return (
     <Modal
@@ -32,11 +33,15 @@ const LoginForm = ({ show, close }) => {
       onHide={close}
       overflow={true}
       className="smaller-devices-modal"
+      size="xs"
     >
       <Modal.Header>
         <Modal.Title>Login</Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        <p style={{ textAlign: "center", margin: "0 0 30px 0" }}>
+          Login using any one of the following methods -
+        </p>
         <div id="firebaseui-auth-container"></div>
       </Modal.Body>
     </Modal>
